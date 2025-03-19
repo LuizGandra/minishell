06a1d1c10c6ea2b0e4d41459e99bcfe421db8034
lhenriqu <luizhapsouza@gmail.com>
2025-03-19 09:10:30 -0300

    refactor: remove unused token handling functions from main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 08:15:36 by lhenriqu          #+#    #+#             */
-/*   Updated: 2025/03/19 09:00:38 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2025/03/19 09:10:04 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,9 @@ int	main(void)
 	handle_error(E_SUCCESS);
 }
 
-static char	*get_token_type(t_token_type type)
-{
-    if (type == PIPE)
-        return ("PIPE");
-    else if (type == SEMICOLON)
-        return ("SEMICOLON");
-    else if (type == WORD)
-        return ("WORD");
-    else if (type == REDIR_IN)
-        return ("REDIRECT_IN");
-    else if (type == REDIR_OUT)
-        return ("REDIRECT_OUT");
-    else if (type == REDIR_OUT_APP)
-        return ("REDIRECT_APPEND");
-    else if (type == REDIR_HDOC)
-        return ("REDIRECT_HEREDOC");
-    else if (type == OPEN_BRACKET)
-		return ("OPEN_BRACKET");
-	else if (type == CLOSE_BRACKET)
-		return ("CLOSE_BRACKET");
-	else if (type == AND)
-		return ("AND");
-	else if (type == OR)
-		return ("OR");
-	else
-		return ("UNKNOWN");
-}
-
-static void	print_tokens(t_token_list *tokens)
-{
-	t_token_list	*tmp;
-
-	tmp = tokens;
-	while (tmp)
-	{
-		ft_printf(C_GRN C_BLD "Token:" C_RST "%s, " C_CYA "type:" C_YEL " %s\n" C_RST, tmp->token.content,
-			get_token_type(tmp->token.type));
-		tmp = tmp->next;
-	}
-}
-
 static void	ft_loop(void)
 {
-	t_shell *shell;
+	t_shell	*shell;
 
 	shell = get_minishell();
 	while (TRUE)
@@ -78,6 +37,5 @@ static void	ft_loop(void)
 		}
 		add_history(shell->user_input);
 		shell->tokens = get_token_list(shell->user_input);
-		print_tokens(shell->tokens);
 	}
 }
