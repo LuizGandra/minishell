234195324980f01273e1f9fa0fa2eb1374f232f3
lhenriqu42 <luizhapsouza@gmail.com>
2025-03-24 15:27:38 -0300

    fix: correct quote length calculation in handle_quote function

--- src/lexing/lexer.c
+++ src/lexing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 07:51:27 by lhenriqu          #+#    #+#             */
-/*   Updated: 2025/03/24 12:49:16 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2025/03/24 15:22:05 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static char	*handle_quote(t_token *node, char *input)
 	if (*input != '\'' && *input != '"')
 		return (input);
 	quote = *input;
-	len = 1;
+	len = 2;
 	while (input[len - 1] && input[len - 1] != quote)
 		len++;
 	node->content[node->size].str = ft_substr(input, 0, len);
