503233f639ea8085af735f7666d817403fe1fb0e
lhenriqu <luizhapsouza@gmail.com>
2025-04-01 13:23:51 -0300

    feat: add execution tree structure and path handling functions

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -1,41 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.h                                           :+:      :+:    :+:   */
+/*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 09:59:41 by lhenriqu          #+#    #+#             */
-/*   Updated: 2025/04/01 07:47:39 by lhenriqu         ###   ########.fr       */
+/*   Created: 2025/04/01 07:44:19 by lhenriqu          #+#    #+#             */
+/*   Updated: 2025/04/01 13:21:48 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_H
-# define PARSER_H
+#include "exec.h"
 
-# include "lexing.h"
-# include "minishell.h"
-
-typedef enum e_tree_type
+int	exec(t_exec_tree *tree)
 {
-	PIPE,
-	AND,
-	OR,
-	REDIR_IN,
-	REDIR_OUT,
-	REDIR_APPEND,
-	REDIR_HEREDOC,
-	COMMAND,
-	SUBSHELL
-}						t_tree_type;
+	int	result;
 
-typedef struct s_exec_tree
-{
-	t_tree_type			type;
-	t_token_list		*command;
-	struct s_exec_tree	*subshell;
-	struct s_exec_tree	*left;
-	struct s_exec_tree	*right;
-}						t_exec_tree;
-
-#endif
+	if (tree->type == SUBSHELL)
+		return (exec(tree->subshell));
+	result = exec(tree->left);
+	if (tree->type == OR && result)
+		result = exec(tree->right);
+	else if (tree->type == AND && !result)
+		result = exec(tree->right);
+		
+	// EXECUTION
+	
+	return (result);
+}
