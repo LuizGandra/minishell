7a78c21e6561c6e65dd0c8cb05246e30a130427e
lhenriqu <luizhapsouza@gmail.com>
2025-04-02 10:18:18 -0300

    refactor: rename tree type enums for improved clarity and consistency

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 09:59:41 by lhenriqu          #+#    #+#             */
-/*   Updated: 2025/04/01 07:47:39 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2025/04/02 08:18:16 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@
 
 typedef enum e_tree_type
 {
-	PIPE,
-	AND,
-	OR,
-	REDIR_IN,
-	REDIR_OUT,
-	REDIR_APPEND,
-	REDIR_HEREDOC,
-	COMMAND,
-	SUBSHELL
+	TREE_PIPE,
+	TREE_AND,
+	TREE_OR,
+	TREE_REDIR_IN,
+	TREE_REDIR_OUT,
+	TREE_REDIR_APPEND,
+	TREE_REDIR_HEREDOC,
+	TREE_COMMAND,
+	TREE_SUBSHELL
 }						t_tree_type;
 
 typedef struct s_exec_tree
 {
 	t_tree_type			type;
 	t_token_list		*command;
+	char				*file;
 	struct s_exec_tree	*subshell;
 	struct s_exec_tree	*left;
 	struct s_exec_tree	*right;
