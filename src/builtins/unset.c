ad5212a34cc76fb770ac9166916c2df9ee71c081
LuizGandra <luizc.gandra29@gmail.com>
2025-05-04 00:29:01 -0300

    style: fix norminette in builtins

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: lcosta-g <lcosta-g@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:33:40 by lcosta-g          #+#    #+#             */
-/*   Updated: 2025/05/02 18:19:19 by lcosta-g         ###   ########.fr       */
+/*   Updated: 2025/05/04 00:28:44 by lcosta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	b_unset(char **args)
 {
-	t_hash_table *env;
-	char **temp;
-	char *str;
+	t_hash_table	*env;
+	char			**temp;
+	char			*str;
 
 	if (!args || !args[1])
 		return (0);
